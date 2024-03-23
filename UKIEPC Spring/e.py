ee1859a84666bc2da08b757eff03427e231caa88
Deyao Chen <chendeyao000@gmail.com>
2024-03-23 14:22:17 +0000

    add e

---
+++ UKIEPC Spring/e.py
@@ -0,0 +1,15 @@
+def passes(time):
+    if time == 0:
+        return 0
+    return time // 720 + 1
+
+t = int(input())
+for _ in range(t):
+    T, d = map(int, input().split())
+    T *= 11
+    d *= 11
+    ans = (T+d+1) // (720 * 11) * 11
+    ans -= passes(T)
+    ans += passes((T+d+1) % (720 * 11)) 
+
+    print(ans)
