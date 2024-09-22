1bce34d406966baddc8c88d81394401bb399acc3
Deyao Chen <chendeyao000@gmail.com>
2024-09-22 17:27:47 +0000

    update dijkstra

--- references/dijkstra.cpp
+++ references/dijkstra.cpp
@@ -15,28 +15,35 @@ template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr
 #define ll long long
 #define ipair pair<ll, ll>
 #define INF LONG_LONG_MAX
-pair<vector<ll>, vector<ll>> dijkstra(ll n, vector<vector<ipair>> edges, ll start) {
-    set<ipair> queue;
-    vector<ll> dist;
-    vector<ll> prev;
-    dist.resize(n, INF);
-    prev.resize(n, INF);
-    dist.at(start) = 0;
-    queue.insert({0, start});
-    while(!queue.empty()) {
-        auto top = queue.begin();
-        auto [d, u] = *top;
-        queue.erase(top);
-        for (auto [w, v] : edges[u]) {
-            ll alt = dist[u] + w;
-            if (alt < dist[v]) {
-                dist[v] = alt;
-                prev[v] = u;
-                queue.insert({alt, v});
+#define MAXN 2e5
+
+vector<vector<ipair>> adj(MAXN);
+vector<ll> d(MAXN);
+vector<ll> p(MAXN);
+
+void dijkstra(ll n, ll s, vector<ll> & d, vector<ll> & p) {
+    d.assign(n, INF);
+    p.assign(n, -1);
+
+    d[s] = 0;
+    set<ipair> q;
+    q.insert({0, s});
+    while (!q.empty()) {
+        int v = q.begin()->second;
+        q.erase(q.begin());
+
+        for (auto edge : adj[v]) {
+            int len = edge.first;
+            int to = edge.second;
+
+            if (d[v] + len < d[to]) {
+                q.erase({d[to], to});
+                d[to] = d[v] + len;
+                p[to] = v;
+                q.insert({d[to], to});
             }
         }
     }
-    return {dist, prev};
 }
 
 int main() {
@@ -45,27 +52,27 @@ int main() {
     cin.tie(0); cout.tie(0);
     ll n, m;
     cin >> n >> m;
-    vector<vector<ipair>> edges;
     for (ll i = 0; i < n; i++) {
-        vector<ipair> vv;
-        edges.push_back(vv);
+        adj.at(i).clear();
     }
     for (ll i = 0; i < m; i++) {
         ll u, v, w;
         cin >> u >> v >> w;
         u--;
         v--;
-        edges.at(u).push_back({w, v});
-        edges.at(v).push_back({w, u});
+        adj.at(u).push_back({w, v});
+        adj.at(v).push_back({w, u});
     }
-    auto [_, prev] = dijkstra(n, edges, 0);
+    dijkstra(n, 0, d, p);
     vector<ll> path;
     ll element = n-1;
-    while (element != 0 && element != INF) {
+    dbg(d);
+    dbg(p);
+    while (element != 0 && element != -1) {
         path.push_back(element);
-        element = prev[element];
+        element = p[element];
     }
-    if (element == INF) {
+    if (element == -1) {
         cout << -1 << endl;
     } else {
         cout << 1 << " ";
